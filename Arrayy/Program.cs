4284b0f7a984eee584961c2efbb7ccf6715f333b
[ismaylibra] <[ismayiliib@code.edu.az]>
2022-04-17 17:52:05 +0400

    modified

--- Arrayy/Program.cs
+++ Arrayy/Program.cs
@@ -6,12 +6,12 @@ namespace Arrayy
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Eded daxil edin: ");
+            Console.Write("Eded daxil edin: ");
             int num = Convert.ToInt32(Console.ReadLine());
             ;
             if (num<50)
             {
-                Console.WriteLine("3-e bolunen ededler: ");
+                Console.Write("3-e bolunen ededler: ");
                 for (int i = 0; i < num; i++)
                 { 
                    
@@ -21,14 +21,13 @@ namespace Arrayy
                         
                     { int num3Size = (num/ 3)+1;
                         int[] num3 = new int[num3Size];
-                        int length = (i / 3);
-                        Console.WriteLine(i);
+                        Console.Write(i+" ");
                     }
                 }
             }
-            if (num>50 && num<100)
+            if (num>50 && num<=100)
             {
-                Console.WriteLine("5- bolunen ededler: ");
+                Console.Write("5- bolunen ededler: ");
 
                 for (int i = 0; i < num; i++)
                 {
@@ -40,14 +39,14 @@ namespace Arrayy
                     {
                         int num5Size = (num / 5) + 1;
                         int[] num3 = new int[num5Size];
-                        int length = (i / 5);
-                        Console.WriteLine(i);
+
+                        Console.Write(i + " ");
                     }
                 }
             }
             if (num>100)
             {
-                 Console.WriteLine("8-e bolunen ededler: ");
+                 Console.Write("8-e bolunen ededler: ");
                 for (int i = 0; i < num; i++)
                 {
 
@@ -58,8 +57,7 @@ namespace Arrayy
                     {
                         int num8Size = (num / 8) + 1;
                         int[] num3 = new int[num8Size];
-                        int length = (i / 8);
-                        Console.WriteLine(i);
+                        Console.Write(i + " ");
                     }
                 }
             }
